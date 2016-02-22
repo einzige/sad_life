ccdac392115a1cf0076f03707f4cdfb0da6094a8
Sergei Zinin <zinin@xakep.ru>
2016-02-22 17:58:41 +0700

    Add node connections to arcs

--- lib/petri/arc.rb
+++ lib/petri/arc.rb
@@ -1,4 +1,12 @@
 module Petri
   class Arc < Element
+    attr_reader :from_node
+    attr_reader :to_node
+
+    def initialize(net, from: nil, to: nil)
+      super(net)
+      @from_node = from
+      @to_node = to
+    end
   end
 end
--- spec/petri/arc_spec.rb
+++ spec/petri/arc_spec.rb
@@ -1,9 +1,19 @@
 describe Petri::Arc do
   let(:net) { Petri::Net.new }
+  let(:from_node) { Petri::Place.new(net) }
+  let(:to_node) { Petri::Transition.new(net) }
 
-  subject { Petri::Arc.new(net) }
+  subject { Petri::Arc.new(net, from: from_node, to: to_node) }
 
   it 'assigns net' do
     subject.net.must_equal net
+  end
+
+  it 'assigns from_node' do
+    subject.from_node.must_equal from_node
+  end
+
+  it 'assigns to_node' do
+    subject.to_node.must_equal to_node
   end
 end
